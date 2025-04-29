01d97532e36fc909ac32ed027b07cfdddd9f6a1b
DrivandiPratama <dripandipratama@gmail.com>
2025-04-29 15:28:36 +0700

    Mengisi Prosedur Data untuk menampilkan Data

--- file1.cpp
+++ file1.cpp
@@ -8,9 +8,12 @@ public :
     float nilai;
 
     void printData(){
-        
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
     }
 };
+
 int main(){
 
 }
